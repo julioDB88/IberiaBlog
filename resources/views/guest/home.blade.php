02c0959d10c7367958c22b2ced84adfbd8819eb5
Julio Desplat <juliodesplat@protonmail.com>
2021-03-11 21:19:59 +0100

    donemigration4

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -16,7 +16,7 @@
     </div>
     <div class="flex">
         @include('guest.includes.latestnews')
-       @include('guest.includes.relatedcontent')
+        @include('guest.includes.relatedcontent')
     </div>
 
 
@@ -24,40 +24,36 @@
         <div class="header-section w-full text-xl md:text-3xl py-8 text-center text-white">
             @lang('main.authors')
         </div>
+        <div class="flex flex-wrap p-6">
+            @foreach (User::all() as $user)
+
+            <div class="h-full text-left bg-indigo-700 rounded p-5 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 m-2">
+                <a class="inline-flex items-center py-6">
+                    @if($user->profile_photo_path)
+                    <img alt="testimonial"
+                        class="inline-block object-cover object-center w-16 h-16 bg-gray-100 rounded-full"
+                        src="{{asset('storage')}}/{{$user->profile_photo_path}}">
+
+                    @else
+                    <img alt="testimonial"
+                        class="inline-block object-cover object-center w-16 h-16 bg-gray-100 rounded-full"
+                        src="{{asset('media')}}/anon.svg">
+                    @endif
+                    <span class="flex flex-col flex-grow pl-4">
+                        <span class="font-medium  title-font text-white font-bold">{{$user->name}}</span>
+                        <span class="text-sm  uppercase text-white ">{{$user->subtitle}}</span>
+                    </span>
+                </a>
+                @if($user->description)
+                <p class="text-base font-medium leading-relaxed text-white">{{$user->description}}</p>
+                @else
+                <p class="text-base font-medium leading-relaxed text-white">@lang('main.author_placeholder')</p>
+                @endif
+            </div>
 
 
-
-                    <div class="flex flex-wrap p-6">
-                        @foreach (User::all() as $user)
-
-                            <div class="h-full text-left bg-indigo-700 rounded p-5 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 m-2">
-                                <a class="inline-flex items-center py-6">
-                                    @if($user->profile_foto_url)
-                                    <img alt="testimonial"
-                                        class="inline-block object-cover object-center w-16 h-16 bg-gray-100 rounded-full"
-                                        src="{{$user->profile_foto_url}}">
-
-                                    @else
-                                    <img alt="testimonial"
-                                    class="inline-block object-cover object-center w-16 h-16 bg-gray-100 rounded-full"
-                                    src="{{asset('media')}}/anon.svg">
-                                    @endif
-                                        <span
-                                        class="flex flex-col flex-grow pl-4">
-                                        <span class="font-medium  title-font text-white font-bold">{{$user->name}}</span>
-                                        <span class="text-sm  uppercase text-white ">{{$user->subtitle}}</span>
-                                    </span>
-                                </a>
-                                @if($user->description)
-                                <p class="text-base font-medium leading-relaxed text-white">{{$user->description}}</p>
-                                @else
-                                <p class="text-base font-medium leading-relaxed text-white">@lang('main.author_placeholder')</p>
-                                @endif
-                            </div>
-
-
-                        @endforeach
-                    </div>
+            @endforeach
+        </div>
 
     </section>
 
